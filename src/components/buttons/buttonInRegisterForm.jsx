7b476e8c377974a8dcfaaa276dc850a45f4dc5eb
Varya <rum.varvara@yandex.ru>
2023-07-02 19:24:13 +0300

    added some verification and registration without backend

--- src/components/buttons/buttonInRegisterForm.jsx
+++
@@ -1,9 +0,0 @@
-import './buttonInRegisterForm.css'
-
-export const ButtonInRegisterForm = () => {
-    return (
-        <>
-            <button className="button_in_register_form"><span className='text_in_button'>Создать аккаунт</span></button>
-        </>
-    )
-}
--- src/components/registerComponent/register.jsx
+++ src/components/registerComponent/register.jsx
@@ -1,34 +1,175 @@
 import './register.css'
 import { Link } from 'react-router-dom'
-import { ButtonInRegisterForm } from '../buttons/buttonInRegisterForm'
+import { useRef, useState, useEffect } from 'react'
+import axios from '../../api/axios';
+
+
+const USER_REGEX = /^[?!,.а-яА-ЯёЁ0-9\s]{2,20}$/; 
+const PWD_REGEX = /^[A-z][A-z0-9-_]{8,20}$/; 
+const EMAIL_REGEX = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
 
 export const RegisterComponent = () => {
+    const userRef = useRef();
+    const errRef = useRef(); 
+
+    const [user, setUser] = useState(''); 
+    const [validName, setValidName] = useState(false);
+    const [userFocus, setUserFocus] = useState(false); 
+
+    const [lastName, setLastName] = useState(''); 
+    const [validLastName, setValidLastName] = useState(false);
+    const [lastNameFocus, setLastNameFocus] = useState(false); 
+
+    const [pwd, setPwd] = useState(''); 
+    const [validPwd, setValidPwd] = useState(false); 
+    const [pwdFocus, setPwdFocus] = useState(false); 
+
+    const [email, setEmail] = useState(''); 
+    const [validEmail, setValidEmail] = useState(false); 
+    const [emailFocus, setEmailFocus] = useState(false); 
+
+    const [errMsg, setErrMsg] = useState(''); 
+    const [success, setSuccess] = useState(false); 
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        const result = USER_REGEX.test(user); 
+        setValidName(result); 
+    }, [user])
+
+    useEffect(() => {
+        const result = EMAIL_REGEX.test(email); 
+        setValidEmail(result); 
+    }, [email])
+
+    useEffect(() => {
+        const result = PWD_REGEX.test(pwd); 
+        setValidPwd(result); 
+    }, [pwd])
+
+    useEffect(() => {
+        const result = USER_REGEX.test(lastName); 
+        setValidLastName(result); 
+    }, [lastName])
+
+    useEffect(() => {
+        setErrMsg(''); 
+    }, [user, pwd])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(user, pwd, lastName, email); 
+        setSuccess(true);
+    }
+
     return (
         <>
+        {success ? (
+            <div className='success_submit_form'>
+                <h1>Вы успешно зарегистрировались!</h1>
+                    <Link to='/login'>Войти</Link>
+            </div>
+        ) : (
            <div className='wrapper_for_register_form'>
             <div className='register_form'>
+                <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}>{errMsg}</p>
                 <h1>Зарегистрироваться</h1>
-                <input className='input_register'
+                <form onSubmit={handleSubmit} className='submit_form'>
+                
+                <label htmlFor='useremail' className='label_register_form'>
+                    <br/>Почтовый ящик:
+                    <span className={validEmail ? "valid" : "hide"}>&#9989;</span>
+                    <span className={validEmail || !email ? "hide" : "invalid"}>&#10060;</span>
+                    <br/> 
+                </label>
+                <input 
+                    className='input_register'
                     type='email'
-                    placeholder='введите почтовый ящик'
+                    placeholder='введите адрес вашего почтового ящика'
+                    id='useremail'
+                    ref={userRef}
+                    autoComplete='off'
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    onFocus={() => setEmailFocus(true)}
+                    onBlur={() => setEmailFocus(false)}
                     ></input>
-                <input className='input_register'
+                    <p className={emailFocus && email && !validEmail ? "instructions" : "offscreen"}>
+                      Некорректно введен адрес почтового ящика
+                    </p>
+
+                <label htmlFor='password' className='label_register_form'>
+                    <br/>Пароль:
+                    <span className={validPwd ? "valid" : "hide"}>&#9989;</span>
+                    <span className={validPwd || !pwd ? "hide" : "invalid"}>&#10060;</span>
+                    <br/>
+                </label>
+                <input 
+                    className='input_register'
                     type='password'
                     placeholder='придумайте пароль'
+                    id='password'
+                    onChange={(e) => setPwd(e.target.value)}
+                    required
+                    onFocus={() => setPwdFocus(true)}
+                    onBlur={() => setPwdFocus(false)}
                 ></input>
-                <input className='input_register'
+                <p className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+                    От 8 и до 20 символов.
+                </p>
+
+                <label htmlFor='username' className='label_register_form'>
+                    <br/>Имя:
+                    <span className={validName ? "valid" : "hide"}>&#9989;</span>
+                    <span className={validName || !user ? "hide" : "invalid"}>&#10060;</span>
+                    <br/> 
+                </label>
+                <input 
+                    className='input_register'
                     type='text'
                     placeholder='введите имя'
+                    id='username'
+                    autoComplete='off'
+                    onChange={(e) => setUser(e.target.value)}
+                    required
+                    onFocus={() => setUserFocus(true)}
+                    onBlur={() => setUserFocus(false)}
                     ></input>
-                <input className='input_register'
+                    <p className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+                      От 2 до 20 символов. Только кириллица. 
+                    </p>
+
+                <label htmlFor='userlastname' className='label_register_form'>
+                    <br/>Фамилия:
+                    <span className={validLastName ? "valid" : "hide"}>&#9989;</span>
+                    <span className={validLastName || !lastName ? "hide" : "invalid"}>&#10060;</span>
+                    <br/> 
+                </label>  
+                <input 
+                    className='input_register'
                     type='text'
                     placeholder='введите фамилию'
+                    id='userlastname'
+                    autoComplete='off'
+                    onChange={(e) => setLastName(e.target.value)}
+                    required
+                    onFocus={() => setLastNameFocus(true)}
+                    onBlur={() => setLastNameFocus(false)}
                 ></input>
+                    <p className={lastNameFocus && lastName && !validLastName ? "instructions" : "offscreen"}>
+                        От 2 до 20 символов. Только кириллица. 
+                    </p>
+                <button className="button_in_register_form" disabled={!validName || !validPwd || !validEmail ? true : false}><span className='text_in_button'>Создать аккаунт</span></button>
+                </form>
+              
                 {/* <label class="checkbox_in_register_form"><input type="checkbox" value="yes" className="checkbox1"></input>Одобрен</label> */}
-                <ButtonInRegisterForm/>
                  <p className='text_with_link'>Есть аккаунт? <Link to='/login'>Войти</Link></p>
             </div>
         </div>
+        )}
         </>
     )
 }
